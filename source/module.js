77678cf87257633f4acce583a5f55683cffed8ba
Priya Hiremath <priyahrish18@gmail.com>
2018-02-19 22:14:34 +1300

    adding module js with update

--- source/module.js
+++ source/module.js
@@ -1,4 +1,25 @@
-const g = require("./module2.js").greeting;
+const http = require('http');
+let dt = require('./module.js');
+
+
+http.createServer(function(req, res){
+    res.writeHead(200,{'Content-Type' :'text/html'});
+    res.write("The date and Time are currently: " + dt.myDateTime() );
+    res.end();
+
+
+
+
+}).listen(3000);
+
+
+
+
+
+
+
+
+/*const g = require("./module2.js").greeting;
 
 console.log(g('priya'));
 
@@ -9,4 +30,5 @@ const getAccounts = () => {
     const request = new XMLHttpRequest();
     request.open("GET", serverURL, true);
     request.send("name='hrishi'");
-};
+};*/
+
